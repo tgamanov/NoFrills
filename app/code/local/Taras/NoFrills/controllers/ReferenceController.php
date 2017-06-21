6743f0a6cbd7bfb5bd050dbaf228864f9489190b
taras <taras@ecomitize.com>
2017-06-21 14:02:36 +0300

    CHAPTER 2. XML PAGE LAYOUT FILES (page 51)

--- app/code/local/Taras/NoFrills/controllers/ReferenceController.php
+++ app/code/local/Taras/NoFrills/controllers/ReferenceController.php
@@ -26,28 +26,50 @@ class Taras_NoFrills_ReferenceController extends Mage_Core_Controller_Front_Acti
 		echo $layout->setDirectOutput( false )->getOutput();
 	}
 
-	public function indexAction()
-	{
+	public function indexAction() {
 		$this->_initLayout();
-		$this->_sendOutput();
-	}
-
-	/*public function indexAction() {
-		$this->_initLayout();
-
 		Mage::getSingleton( 'core/layout' )
 		    ->getUpdate()
 		    ->addUpdate( '
-                <reference name="content">
-                    <block type="core/text" name="our_message">
-                        <action method="setText">
-                            <text>Here we go!</text>
-                        </action>
-                    </block>
-                </reference>' );
-
+				<reference name="content">
+					<block type="core/text" name="our_message">
+						<action method="setText"><text>Here we go!</text></action>
+					</block>
+				</reference>
+' );
 		$this->_sendOutput();
-	}*/
+	}
 
+	protected function _loadUpdateFile( $file ) {
+		$path_update = Mage::getModuleDir( '', 'Taras_NoFrills' ) . DS . 'content-updates' . DS . $file;
+		$layout      = Mage::getSingleton( 'core/layout' )
+		                   ->getUpdate()
+		                   ->addUpdate( file_get_contents( $path_update ) );
+	}
+
+	public function foxAction() {
+		$this->_initLayout();
+		$this->_loadUpdateFileFromRequest( 'fox.xml' );
+		$this->_sendOutput();
+	}
+
+	public function dogAction() {
+		$this->_initLayout();
+		$this->_loadUpdateFile( 'dog.xml' );
+		$this->_sendOutput();
+	}
+
+	public function ceaserAction() {
+		$this->_initLayout();
+		$this->_loadUpdateFile( 'ceaser.xml' );
+		$this->_sendOutput();
+	}
+
+	protected function _loadUpdateFileFromRequest() {
+		$path_update = Mage::getModuleDir( '', 'Taras_NoFrills' ) . DS . 'content-updates' . DS . $this->getFullActionName() . '.xml';
+		$layout      = Mage::getSingleton( 'core/layout' )
+		                   ->getUpdate()
+		                   ->addUpdate( file_get_contents( $path_update ) );
+	}
 
 }
